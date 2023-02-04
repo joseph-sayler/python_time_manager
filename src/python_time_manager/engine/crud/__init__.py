af7cf54de0e4cadc01ecffc069c5121da56cd9e3
Joseph Sayler <joseph.sayler@gmail.com>
2023-02-03 20:21:47 -0800

    removed individual files for crud commands

--- src/python_time_manager/engine/crud/__init__.py
+++
@@ -1,8 +0,0 @@
-from src.python_time_manager.models import User, Project, Event
-
-
-TABLE_STRUCT: dict[str, User | Project | Event] = {
-    "user": User,
-    "project": Project,
-    "event": Event,
-}
